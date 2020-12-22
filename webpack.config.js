0bbe5c0e74ace15ebacc655c746370057f26b703
timoransky <j.timoransky@gmail.com>
2020-12-22 19:38:22 +0100

    js ie11 support

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,37 @@ const path = require('path');
 
 module.exports = {
     entry: './src/js/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    targets: "defaults",
+                                    useBuiltIns: "usage",
+                                    corejs: 3
+                                }
+                            ]
+                        ],
+                        plugins: [
+                            [
+                                "@babel/plugin-transform-runtime",
+                                {
+                                    "corejs": 3
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, 'dist'),
